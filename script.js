51b344296dab0365fefc9a9578eb3c678f34351f
Kantesh Prasad H K <hk.kanteshprasad@gmail.com>
2024-08-03 13:47:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+
+// script.js
+
+window.addEventListener("load", function() {
+    // When the page is fully loaded, including all images and sub-resources
+    var spinner = document.getElementById('spinner');
+    var content = document.getElementById('content');
+    
+    if (spinner && content) {
+        spinner.style.display = 'none';
+        content.style.display = 'block';
+    }
+});
+
+
+
+
 setTimeout(function() {
     var overlay = document.querySelector('.overlay');
     if (overlay) {
         overlay.parentNode.removeChild(overlay);
     }
 }, 6000);
+
+
+
